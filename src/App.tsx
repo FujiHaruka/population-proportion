4a03d478250a9376511031b8da230c6991c93933
FujiHaruka <fizzyjohn1427@gmail.com>
2022-06-19 13:56:23 +0900

    impl calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import styles from "./App.module.css";
+
+type ConfidenceInterval = 0.9 | 0.95 | 0.99;
+const CIFormats = new Map<ConfidenceInterval, string>([
+  [0.9, "99%"],
+  [0.95, "95%"],
+  [0.99, "99%"],
+]);
+const CIValues = new Map<ConfidenceInterval, number>([
+  [0.9, 1.64],
+  [0.95, 1.96],
+  [0.99, 2.58],
+]);
+
+type State = {
+  sampleSizeRangeValue: number;
+  successesRangeValue: number;
+  confidenceInterval: number;
+};
 
 const App: Component = () => {
+  const [sampleSizeValue, setSampleSize] = createSignal(40);
+  const sampleSize = () => Math.floor(Math.pow(10, sampleSizeValue() / 20));
+  const [successes, setSuccesses] = createSignal(50);
+  const [confidenceInterval, setConfidenceInterval] =
+    createSignal<ConfidenceInterval>(0.95);
+
+  const lowerBound = () => {
+    const p = successes() / 100;
+    const z = CIValues.get(confidenceInterval())!;
+    const diff = z * Math.sqrt((p * (1 - p)) / sampleSize());
+    return p - diff;
+  };
+  const upperBound = () => {
+    const p = successes() / 100;
+    const z = CIValues.get(confidenceInterval())!;
+    const diff = z * Math.sqrt((p * (1 - p)) / sampleSize());
+    return p + diff;
+  };
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <>
+      <header class="container">
+        <h3>Interval Estimate of Population Proportion</h3>
       </header>
-    </div>
+      <main class="container">
+        <form>
+          <label for="sample-size">
+            Sample size: <b>{sampleSize()}</b>
+            <input
+              type="range"
+              min="0"
+              max="100"
+              value={sampleSizeValue()}
+              onChange={(ev) => {
+                setSampleSize(Number(ev.currentTarget.value));
+              }}
+              id="sample-size"
+            />
+          </label>
+          <label for="successes">
+            Successes: <b>{successes()}%</b>
+            <input
+              type="range"
+              min="0"
+              max="100"
+              value={successes()}
+              onChange={(ev) => {
+                setSuccesses(Number(ev.currentTarget.value));
+              }}
+              id="successes"
+            />
+          </label>
+          <label for="confidence-interval">
+            Confidence Interval: <b>{CIFormats.get(confidenceInterval())}</b>
+            <select
+              id="confidence-interval"
+              onChange={(ev) => {
+                setConfidenceInterval(
+                  Number(ev.currentTarget.value) as ConfidenceInterval
+                );
+              }}
+            >
+              {[...CIFormats.keys()].map((value) => (
+                <option value={value}>{CIFormats.get(value)}</option>
+              ))}
+            </select>
+          </label>
+        </form>
+        <div>
+          <h3>Result</h3>
+          <div>
+            {lowerBound().toFixed(3)} - {upperBound().toFixed(3)}
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
