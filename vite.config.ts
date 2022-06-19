c43aa4cb750e8ea21722b41e08575f475ba87005
FujiHaruka <fizzyjohn1427@gmail.com>
2022-06-19 14:45:12 +0900

    fix vite base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: "/population-proportion/",
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
